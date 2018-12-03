6f7e6cee4f4374a4bcb67a3aeab1a56fed80c7d7
Miguel Angel Covarrubias <Flatcova@users.noreply.github.com>
2018-12-03 15:31:24 -0600

    trying routing

--- client/main.js
+++ client/main.js
@@ -3,12 +3,12 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.mainTemplate.onCreated(function helloOnCreated() {
+Template.mainTemplate.onCreated({
 
 });
 
 Template.mainTemplate.helpers({
-  
+
 });
 
 Template.mainTemplate.events({
